8a9551e446b73ff9f1e1d0e1c7368861b8ad7e5e
leedhfsd <leedhfsd@gmail.com>
2023-03-21 21:26:01 +0900

    nodemailer promise

--- pages/api/auth/mail.ts
+++ pages/api/auth/mail.ts
@@ -10,39 +10,47 @@ export default async function handler(
   const { email } = req.query;
   const authCode = Math.random().toString().substring(2, 8);
   let status = true;
-  let form;
+  let form: string;
   if (typeof email === "string" && validateEmail(email)) {
-    renderFile(
-      "./lib/form.ejs",
-      { authCode, name: email.split("@")[0] },
-      (err, data) => {
-        form = data;
-      },
-    );
-  }
-  const transporter = createTransport({
-    host: process.env.EMAIL_SERVER_HOST,
-    port: process.env.EMAIL_SERVER_PORT,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_SERVER_USER,
-      pass: process.env.EMAIL_SERVER_PASSWORD,
-    },
-  });
-  const mailOptions = {
-    from: "no_reply@allog.com",
-    to: email,
-    subject: "[Allog] 인증번호 안내 메일",
-    html: form,
-  };
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        reject(err);
-        status = false;
-      } else resolve(info);
+    await new Promise((resolve, reject) => {
+      renderFile(
+        "./lib/form.ejs",
+        { authCode, name: email.split("@")[0] },
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            form = data;
+            resolve(data);
+          }
+        },
+      );
+    }).then(async () => {
+      const transporter = createTransport({
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      });
+      const mailOptions = {
+        from: "no_reply@allog.com",
+        to: email,
+        subject: "[Allog] 인증번호 안내 메일",
+        html: form,
+      };
+      await new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, (err, info) => {
+          if (err) {
+            reject(err);
+            status = false;
+          } else resolve(info);
+        });
+      });
     });
-  });
+  }
   if (!status) {
     return res.status(500).send({ error: "failed to send email" });
   }
