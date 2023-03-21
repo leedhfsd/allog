4460619d9d08c19adacca30e9d33d5064be64375
leedhfsd <leedhfsd@gmail.com>
2023-03-21 21:18:10 +0900

    nodemailer test

--- pages/api/auth/mail.ts
+++ pages/api/auth/mail.ts
@@ -3,7 +3,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { createTransport } from "nodemailer";
 import { validateEmail } from "../../../lib/validation";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   const { email } = req.query;
   const authCode = Math.random().toString().substring(2, 8);
   let status = true;
@@ -14,32 +17,34 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       { authCode, name: email.split("@")[0] },
       (err, data) => {
         form = data;
-        const transporter = createTransport({
-          host: process.env.EMAIL_SERVER_HOST,
-          port: process.env.EMAIL_SERVER_PORT,
-          secure: true,
-          auth: {
-            user: process.env.EMAIL_SERVER_USER,
-            pass: process.env.EMAIL_SERVER_PASSWORD,
-          },
-        });
-        const mailOptions = {
-          from: "leedhfsd@gmail.com",
-          to: email,
-          subject: "[Allog] 인증번호 안내 메일",
-          html: form,
-        };
-
-        transporter.sendMail(mailOptions, (e) => {
-          if (e) {
-            status = false;
-          }
-        });
-        if (!status) {
-          return res.status(500).send({ error: "failed to send email" });
-        }
-        return res.status(200).send({ ok: "Mail send completed" });
       },
     );
   }
+  const transporter = createTransport({
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: "no_reply@allog.com",
+    to: email,
+    subject: "[Allog] 인증번호 안내 메일",
+    html: form,
+  };
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        reject(err);
+        status = false;
+      } else resolve(info);
+    });
+  });
+  if (!status) {
+    return res.status(500).send({ error: "failed to send email" });
+  }
+  return res.status(200).send({ ok: "mail send completed" });
 }
