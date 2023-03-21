0019dbc7cb251c092f2ed07e1b742569789c7e7d
leedhfsd <leedhfsd@gmail.com>
2023-03-22 00:58:53 +0900

    error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -106,7 +106,7 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
