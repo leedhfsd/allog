8d945ab431ee381c541796ce289dc6a4b6b631ce
leedhfsd <leedhfsd@gmail.com>
2023-03-22 15:19:16 +0900

    nodemailer

--- pages/api/auth/mail.ts
+++ pages/api/auth/mail.ts
@@ -1,8 +1,19 @@
 /* eslint-disable no-console */
-import { renderFile } from "ejs";
+import { render } from "ejs";
 import { NextApiRequest, NextApiResponse } from "next";
 import { createTransport } from "nodemailer";
 import { createHash, validateEmail } from "../../../lib/validation";
+
+const html = `<html>
+<body>
+  <div style="font-family: Arial, Helvetica, sans-serif; width: 500px; margin: 0 auto; text-align: center; border: 1px solid #ccc; padding: 20px;">
+    <h1 style="font-size: 24px;">회원가입 인증번호 안내<h1>
+    <p style="font-weight: normal; font-size: 16px; margin: 0;"><span style="font-weight: bold"><%= name %></span>님 안녕하세요.</p>
+    <p style="font-weight: normal; font-size: 16px; margin: 0;">아래의 인증번호를 입력하여 인증을 완료해주세요.</p>
+    <span style="color: #0EA5E9; display: inline-block; border: 1px solid #0EA5E9; margin-top: 30px; padding: 10px 40px; font-weight: bold; font-size: 16px;"><%= randomDigits %></span>
+  </div>
+</body>
+</html>`;
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,21 +24,7 @@ export default async function handler(
   const authCode = createHash(randomDigits);
   let form;
   if (typeof email === "string" && validateEmail(email)) {
-    await new Promise((resolve, reject) => {
-      renderFile(
-        "./lib/form.ejs",
-        { randomDigits, name: email.split("@")[0] },
-        (err, data) => {
-          if (err) {
-            console.log(err);
-            reject(err);
-          } else {
-            form = data;
-            resolve(data);
-          }
-        },
-      );
-    });
+    form = render(html, { randomDigits, name: email.split("@")[0] });
   }
   const transporter = createTransport({
     host: process.env.EMAIL_SERVER_HOST,
